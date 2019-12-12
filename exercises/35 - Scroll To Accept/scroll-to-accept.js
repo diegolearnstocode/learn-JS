e06c4eca003ea0273f517d6f67b0cd635ffff99e
Diego van Dyk <diegolearnstocode@gmail.com>
2019-12-12 00:15:39 -0300

    shows and hides items, depending on scroll

--- exercises/35 - Scroll To Accept/scroll-to-accept.js
+++ exercises/35 - Scroll To Accept/scroll-to-accept.js
@@ -1,1 +1,14 @@
-console.log('IT WORKS!');
+const terms = document.querySelector('.terms-and-conditions');
+const accept = document.querySelector('button.accept');
+const finish = document.querySelector('.finished');
+
+function showButton() {
+        accept.removeAttribute('disabled');
+}
+function obCallback(payload) {
+        if (payload[0].isIntersecting) {
+                showButton();
+        }
+}
+const ob = new IntersectionObserver(obCallback);
+ob.observe(finish);
