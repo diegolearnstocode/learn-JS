4e9272c9b3e64600c66a41bc122030147e81321e
Diego van Dyk <diegolearnstocode@gmail.com>
2019-12-11 23:45:15 -0300

    fixes image description in modal

--- exercises/34 - Click Outside/click-outside.js
+++ exercises/34 - Click Outside/click-outside.js
@@ -1,44 +1,44 @@
-const cardButtons = document.querySelectorAll('.card button');
 const modalOuter = document.querySelector('.modal-outer');
 const modalInner = document.querySelector('.modal-inner');
+const learnMoreButtons = document.querySelectorAll('.card button');
 
-function handleCardButtonClick(event) {
-  const button = event.currentTarget;
-  const card = button.closest('.card');
-  // Grab the image src
-  const imgSrc = card.querySelector('img').src;
-  const desc = card.dataset.description;
-  const name = card.querySelector('h2').textContent;
-  // populate the modal with the new info
-  modalInner.innerHTML = `
-    <img width="600" height="600" src="${imgSrc.replace(
-      '200',
-      '600'
-    )}" alt="${name}"/>
-    <p>${desc}</p>
-  `;
-  // show the modal
-  modalOuter.classList.add('open');
+function handleCardButtonClick(button) {
+        const selectedCard = button.target.closest('.card');
+        fillInnerModal(selectedCard);
 }
 
-cardButtons.forEach(button =>
-  button.addEventListener('click', handleCardButtonClick)
-);
+function fillInnerModal(card) {
+        const modalImage = document.createElement('img');
+        const imageTitle = document.createElement('p');
+        modalImage.src = card.querySelector('img').src;
+        modalImage.alt = card.dataset.description;
+        modalInner.innerHTML = '';
+        modalInner.prepend(modalImage);
+        modalInner.appendChild(imageTitle);
+        imageTitle.innerText = `${modalImage.alt}`;
+        modalOuter.classList.add('open');
+}
 
 function closeModal() {
-  modalOuter.classList.remove('open');
+        modalOuter.classList.remove('open');
 }
 
-modalOuter.addEventListener('click', function(event) {
-  const isOutside = !event.target.closest('.modal-inner');
-  if (isOutside) {
-    closeModal();
-  }
+document.addEventListener('click', function(e) {
+        if (e.target === modalOuter) {
+                closeModal();
+        }
 });
 
-window.addEventListener('keydown', event => {
-  console.log(event);
-  if (event.key === 'Escape') {
-    closeModal();
-  }
+document.addEventListener('keyup', function(e) {
+        if (e.key === 'Escape') {
+                closeModal();
+        }
 });
+
+learnMoreButtons.forEach(button => {
+        button.addEventListener('click', handleCardButtonClick);
+});
+
+// Grab the image src
+// populate the modal with the new info
+// show the modal
